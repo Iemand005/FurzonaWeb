44e92a5fdaaf5bc19340da5c92f54d4b21eb55ac
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-08-15 20:58:50 +0200

    tab ndent

--- sw.js
+++ sw.js
@@ -1,25 +1,25 @@
 const CACHE_NAME = 'app-cache-v1.0';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/login.html',
-  '/profile.html',
-  '/search.html',
-  '/style.css'
+	'/',
+	'/index.html',
+	'/login.html',
+	'/profile.html',
+	'/search.html',
+	'/style.css'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS);
-    })
-  );
+	event.waitUntil(
+		caches.open(CACHE_NAME).then(cache => {
+			return cache.addAll(ASSETS);
+		})
+	);
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+	event.respondWith(
+		caches.match(event.request).then((response) => {
+			return response || fetch(event.request);
+		})
+	);
 });
