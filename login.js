d78c3193f84910b0955d80c2b951f6f254bc28ec
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-08-15 16:22:35 +0200

    forms ubmit

--- login.js
+++ login.js
@@ -1,5 +1,6 @@
 
 const loginForm = document.getElementById("login-form");
+const loginForm = document.getElementById("login-form");
 
 if (loginForm instanceof HTMLFormElement) {
 	loginForm.onsubmit = (ev) => {
