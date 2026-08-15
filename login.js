02312b47a20216ea803432351e555aff7a312403
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-08-15 16:25:01 +0200

    oh oops mybad

--- login.js
+++ login.js
@@ -1,6 +1,5 @@
 
 const loginForm = document.getElementById("login-form");
-const loginForm = document.getElementById("login-form");
 
 if (loginForm instanceof HTMLFormElement) {
 	loginForm.onsubmit = ev => {
