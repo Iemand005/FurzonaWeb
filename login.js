494912ed6ebbf9c55b3fb62f6e7e351c4221900f
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-08-15 16:24:01 +0200

    formdata hmm!

--- login.js
+++ login.js
@@ -3,7 +3,13 @@ const loginForm = document.getElementById("login-form");
 const loginForm = document.getElementById("login-form");
 
 if (loginForm instanceof HTMLFormElement) {
-	loginForm.onsubmit = (ev) => {
+	loginForm.onsubmit = ev => {
 		ev.preventDefault();
+
+		const formData = new FormData(e.target);
+		const email = formData.get('email');
+		const password = formData.get('password');
+		
+		console.log(email, password);
 	};
 }
