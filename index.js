82a205e9b51b50f8bcfcee3a77430394f44d52e3
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-08-15 16:53:54 +0200

    create and add list items

--- index.js
+++ index.js
@@ -2,5 +2,9 @@
 const postList = document.getElementById("posts-list");
 
 if (postList instanceof HTMLUListElement) {
-	
+	const listItem = document.createElement("li");
+
+
+
+	postList.appendChild(listItem);
 }
