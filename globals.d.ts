14f7a699766e65e3bf2173e6df216573c6304da6
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-08-17 18:10:27 +0200

    thing better stings

--- globals.d.ts
+++ globals.d.ts
@@ -4,7 +4,14 @@
 
 type Method = "GET" | "POST" | "DELETE" | "PUT";
 
+export type ReadEndpointFn = <K extends keyof ApiEndpoints>(
+  endpoint: K | [K, ...string[]]
+) => Promise<ApiEndpoints[K]["response"]>;
 
+export type WriteEndpointFn = <K extends keyof ApiEndpoints>(
+  endpoint: K | [K, ...string[]],
+  body?: ApiEndpoints[K]["body"]
+) => Promise<ApiEndpoints[K]["response"]>;
 
 // Furzona API
 
