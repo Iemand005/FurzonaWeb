463d56756605c4d1f522fb3516eda43dbf1b779a
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-08-18 13:32:54 +0200

    it's 404 discriminating  still

--- Furzona.js
+++ Furzona.js
@@ -430,7 +430,7 @@ class Furzona extends RequestService {
 		const methods = ["GET", "POST", "PUT", "DELETE"];
 		
 		return Promise.all(methods.map(method => 
-			this.requestWithCare(endpoint, method).then(() => ({ method, success: true })).catch((/**@type {FurzonaError}*/reason) => ({ method, success: !(reason.status === 404 || reason.status === 429) && reason.code !== -1 }))
+			this.requestWithCare(endpoint, method).then(() => ({ method, success: true })).catch((/**@type {FurzonaError}*/reason) => ({ method, success: reason.status !== 429 && reason.code !== -1 }))
 		)).then(results => results.filter(r => r.success).map(v => v.method));
 	}
 	/**
